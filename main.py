4d286179befa08a102ce134e97b1b02d88dfd131
davyken <davykennang552@gmail.com>
2026-04-10 21:02:38 +0100

    added content

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ sys.path.insert(0, '/usr/lib/python3.12')
 from src.bot import main
 import asyncio
 
+async def run():
+    await main()
+
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        loop.run_until_complete(main())
-    finally:
-        loop.close()
+    with asyncio.Runner() as runner:
+        runner.run(run())
--- run_bot.py
+++ run_bot.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-import sys
 
 if __name__ == "__main__":
     from src.bot import main
     import asyncio
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        loop.run_until_complete(main())
-    finally:
-        loop.close()
+    
+    async def run():
+        await main()
+    
+    with asyncio.Runner() as runner:
+        runner.run(run())
